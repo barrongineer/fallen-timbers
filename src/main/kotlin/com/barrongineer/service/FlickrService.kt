9b02b5e429d33b485b77b6292446f65c19cdfdc1
Unknown <shaunnb@gmail.com>
2017-05-08 14:52:44 -0400

    Using higher res flickr images.

--- src/main/kotlin/com/barrongineer/service/FlickrService.kt
+++ src/main/kotlin/com/barrongineer/service/FlickrService.kt
@@ -59,7 +59,7 @@ class FlickrService(val restTemplate: RestTemplate,
     }
 
     private fun buildPhotoUrl(photo: FlickrPhoto): String {
-        return "https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg"
+        return "https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg"
     }
 
     private fun getCollections(): FlickrCollectionsResponse {
